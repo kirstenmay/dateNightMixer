d411924ce6ab40abb08eb3a30776c4f4b89f019f
Kirsten Lindblom <kirsten.m.lindblom@gmail.com>
2021-05-24 10:38:18 -0700

    Starter

---
+++ src/main/java/com/bandk/dateproject/DateNightMixerApplication.java
@@ -0,0 +1,13 @@
+package com.bandk.dateproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateNightMixerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateNightMixerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bandk/dateproject/DateNightMixerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandk.dateproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateNightMixerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
